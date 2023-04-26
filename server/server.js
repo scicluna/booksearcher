579cf12bba9ccd93444a9408d161e25426f3252a
scicluna <88948700+scicluna@users.noreply.github.com>
2023-04-26 16:48:18 -0400

    trying to help heroku

--- server/server.js
+++ server/server.js
@@ -19,11 +19,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+app.use(express.static(path.join(__dirname, '../client/build')));
 
-// app.use(routes);
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
